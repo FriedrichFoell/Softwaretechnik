f3d724f8f4291e81e0ff6da6faa7374b0818cbbc
FriedrichFoell <86483970+FriedrichFoell@users.noreply.github.com>
2021-06-25 17:28:58 +0200

    Rename HonorarRechnerTest.java to Testing/HonorarRechnerTest.java

--- Testing/HonorarRechnerTest.java
+++ Testing/HonorarRechnerTest.java
@@ -13,17 +13,17 @@ class HonorarRechnerTest {
 	@Test
 	void testberechneHonorar() {
 		//Berechnung ohne Leerzeichen
-		assertEquals(0.179,HonorarRechner.berechneHonorar("WICHTIG: F¸r Aufgabe 3 und 4 kˆnnen sie ihr eigenes Problem und ihre eigene Programmiersprache und ihr eigenes Lieblings(mock)fragmework nehmen! Hauptsache es macht Spaﬂ und sie tanken nochmal die Prinzipien! "));
+		assertEquals(0.179,HonorarRechner.berechneHonorar("WICHTIG: F√ºr Aufgabe 3 und 4 k√∂nnen sie ihr eigenes Problem und ihre eigene Programmiersprache und ihr eigenes Lieblings(mock)fragmework nehmen! Hauptsache es macht Spa√ü und sie tanken nochmal die Prinzipien! "));
 		//Berechnung bei leerem String
 		assertEquals(0,HonorarRechner.berechneHonorar(""));
 		//Berechnung bei String = null
 		Assertions.assertThrows(NullPointerException.class,()->{HonorarRechner.berechneHonorar(null);});
-		//Berechnung bei Wiederholung. Dieser Test schl‰gt fehl da Ich nicht in der Lage war die passende Methode zu programmieren.
+		//Berechnung bei Wiederholung. Dieser Test schl√§gt fehl da Ich nicht in der Lage war die passende Methode zu programmieren.
 		assertEquals(0,HonorarRechner.berechneHonorar("Dieser Satz ist keine Kopie. Dieser Satz ist keine Kopie"));
 	}
 	@Test
-	void testz‰hleBilder() {
-		//z‰hle Bilder
-		assertEquals(2,HonorarRechner.z‰hleBilder("Das ist ein test text. er enth‰lt ein picture und noch ein picture"));
+	void testz√§hleBilder() {
+		//z√§hle Bilder
+		assertEquals(2,HonorarRechner.z√§hleBilder("Das ist ein test text. er enth√§lt ein picture und noch ein picture"));
 	}
 }
